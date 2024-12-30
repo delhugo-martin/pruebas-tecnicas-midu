63263faf889a45ae2fa6d7f7b0ca34ff08f02af9
delhugomartin <delhugomartin@gmail.com>
2024-12-30 00:22:18 -0300

    carga de los libros en el localstorage al inicio de la aplicacion y se agrega  funcionalidad de agragar libro a la lista de deseos

--- pruebas/01-reading-list/delhugo-martin/reading_list/src/app/app.component.ts
+++ pruebas/01-reading-list/delhugo-martin/reading_list/src/app/app.component.ts
@@ -1,44 +1,91 @@
 import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import books from './assets/docs/books.json'
+import books from './assets/docs/books.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'reading_list';
 
-  library=books.library // libros del json
+  library = books.library; // libros del json
 
-  libros:string|null = this.getLibraryLS()
-  libross:any[] = [];
+  librosArray: any
 
-  constructor(){
-    this.setLibraryLS()
-    this.getLibraryLS()
+  wishListArrayPrint:any[]=[];
 
-  // valido y busco guardar la lista de libros en una variable
-  if(this.libros !== null){this.libross = JSON.parse(this.libros)}
+
+  constructor() {
+    // comprobamos si el localstorage esta en null, si lo está carga los datos en el mismo
+    this.initLS();
+
+    // pasamos el string de los libros y de la wishlist a array para pintarlos
+    this.getLibraryLS();
+    this.getWishListLS();
+  }///
+
+
+
+  ////////////////////////////////////////////////////////////
+  //inicializacion, carga de library en el local storage
+  initLS() {
+    // si el local storage es null entonces carga los libros
+    if (localStorage.getItem('libros') == null) {
+      localStorage.setItem('libros', JSON.stringify(this.library));
+    }
+  }
+
+  // carga de libros del local storage al print
+  getLibraryLS() {
+    let libros = localStorage.getItem('libros');
+    if(libros !== null){
+    // si o si hay que hacer esta verificacion, si no me da error de tipo
+    this.librosArray = JSON.parse(libros);}
+  }
+
+  getWishListLS(){
+    let wishList = localStorage.getItem('wishList');
+    if(wishList !== null){
+      this.wishListArrayPrint = JSON.parse(wishList)
+      //console.log("la wishlist orint es")
+      //console.log(this.wishListArrayPrint)
+    }
+  }
+
+  //////////////////////////////////////////////////////////////
+  // eventos
+
+  addToWishList(book: string) {
+    this.getWishListLS()
+
+    // vamos a comprobar si el libro ya esta en la wishlist
+    let verfBook = this.wishListArrayPrint.find((b)=> b === book)
+    console.log(this.wishListArrayPrint)
+
+    console.log("el libro si sta")
+    console.log(verfBook)
+
+    // conviene trabajar con el array
+    // primero guardo el objeto book en el array wishListArray
+    this.wishListArrayPrint.push(book)
+        //console.log("w arrary") OK
+        //console.log(this.wishListArray)
+
+    // convierto el array el string
+    let wishListStr = JSON.stringify(this.wishListArrayPrint)
+        //console.log("w string")
+        //console.log(wishListStr)
+        //console.log(typeof(wishListStr)) OK
+
+    localStorage.setItem('wishList',wishListStr)
+    this.getWishListLS()
 
   }
 
-  // set localstorage
-  setLibraryLS(){
-    localStorage.setItem("libros",JSON.stringify(this.library))
-    localStorage.setItem("wishList","")
-  }
-
-  // get localstore
-  getLibraryLS(): string|null{
-    let libros=localStorage.getItem("libros")
-    return libros
-  }
-
-
-
+  removeToWishList(isbn: string) {}
 }
